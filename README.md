8b7f0ce73b890ef0917c6fcbae8e6e2196aaa8cf
Na Jongwoo <njw1204@naver.com>
2022-07-21 11:01:02 +0900

    Close #1

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # problem-solving-template
 
 ## C++
+
 Source: [main.cpp](https://github.com/njw1204/problem-solving-template/blob/master/main.cpp)
 
-Requirements: C++14 or above, `bits/stdc++.h` header file
+Requirements: C++11 or above, `bits/stdc++.h` header file
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
  */
 
 #if defined(__GNUC__)&&!defined(__llvm__)
-#pragma GCC target("avx2")
+#pragma GCC target("avx2") // Remove this line if a runtime error occurs
 #pragma GCC optimize("O3,unroll-loops")
 #endif
 #include <bits/stdc++.h>
@@ -31,12 +31,12 @@ typedef pair<ull, ull> pul;
 #define UNSYNC ios_base::sync_with_stdio(false);cin.tie(nullptr)
 #define READTXT for(ifstream __READTXT(STDIN);__READTXT&&__READTXT.get()!=-1&&__CC(freo,pen)(STDIN,"r",stdin);)__READTXT.close()
 #define STDIN "input.txt" // Change a input file path
-template<typename T> const T& MIN(const T& arg) { return arg; } template<typename T, typename... Ts> const auto& MIN(const T& arg, const Ts&... args) { const auto& m = MIN(args...); return !(m < arg) ? arg : m; }
-template<typename T> const T& MAX(const T& arg) { return arg; } template<typename T, typename... Ts> const auto& MAX(const T& arg, const Ts&... args) { const auto& m = MAX(args...); return (arg < m) ? m : arg; }
+template<typename T> const T& MIN(const T& arg) { return arg; } template<typename T, typename... Ts> const T& MIN(const T& arg, const Ts&... args) { return min(arg, MIN(args...)); }
+template<typename T> const T& MAX(const T& arg) { return arg; } template<typename T, typename... Ts> const T& MAX(const T& arg, const Ts&... args) { return max(arg, MAX(args...)); }
 template<typename T> void INPUT(T& arg) { cin >> arg; } template<typename T, typename... Ts> void INPUT(T& arg, Ts&... args) { cin >> arg; INPUT(args...); }
 template<typename T> void PRINT(const T& arg) { cout << arg << '\n'; } template<typename T, typename... Ts> void PRINT(const T& arg, const Ts&... args) { cout << arg << ' '; PRINT(args...); }
 ull GCD(ull a, ull b) { while (b != 0) { ull t = a % b; a = b; b = t; } return a; }
-ull LCM(ull a, ull b) { return a * b / GCD(a, b); }
+ull LCM(ull a, ull b) { return a * b ? a * b / GCD(a, b) : 0; }
 ull POW(ull a, ull b, ull m = ULLONG_MAX) { ull r = 1; while (b) { if (b & 1) r = r * a % m; b >>= 1; a = a * a % m; } return r; }
 
 int main() {
